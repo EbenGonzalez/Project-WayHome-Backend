ca835d587d3a6aea6594099de5a26b29a774cd0e
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-12-05 12:38:51 +0000

    database/relations

--- database/relations.js
+++ database/relations.js
@@ -1,8 +1,26 @@
-
+const User = require('../api/models/users.models')
+const Pet = require('../api/models/pet.models')
+const Comment = require('../api/models/comments.models')
+const History = require('../api/models/history.models')
+const Race = require('../api/models/races.models')
+const Species = require('../api/models/species.models')
 
 function addRelationsToModels() {
   try {
-    
+    User.hasMany(Pet)
+    Pet.belongsTo(User)
+
+    User.hasMany(Comment)
+    Comment.belongsTo(User)
+
+    User.hasMany(History)
+    History.belongsTo(User)
+
+    Race.hasMany(Pet)
+    Pet.belongsTo(Race)
+
+    Species.hasMany(Race)
+    Race.belongsTo(Species)
   } catch (error) {
     throw error
   }
