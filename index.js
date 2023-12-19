1c59022a2d15227940992070218f12e32087ec96
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-12-19 19:29:43 +0000

    alter craxy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const morgan = require('morgan')
 async function checkAndSyncMysql() {
   await checkConnection()
   addRelationsToModels()
-  await syncModels()
+  await syncModels('alter')
 }
 
 function initializeAndListenWithExpress() {
